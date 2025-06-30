b19766c574e8e6bbaf6f3be9f2d6d68c5535cf2b
Mohamed Samir <mos3doun26@gmail.com>
2025-06-30 07:12:25 +0300

    Set the project base

---
+++ data.js
@@ -0,0 +1,23 @@
+const menuArray = [
+    {
+        name: "Pizza",
+        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        id: 0,
+        price: 14,
+        emoji: "🍕"
+    },
+    {
+        name: "Hamburger",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        emoji: "🍔",
+        id: 1
+    },
+    {
+        name: "Orange Juice",
+        ingredients: ["suger", "orange", "water"],
+        price: 6,
+        emoji: "🍊",
+        id: 2
+    }
+]
